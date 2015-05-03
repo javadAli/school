40ebb2f1dcdb3b71aeacaba5fa8dbfdc237d379f
javadAli <javadaliakhlaqi@yahoo.com>
2015-05-02 22:36:06 -0700

    Create leftmenue.php

---
+++ leftmenue.php
@@ -0,0 +1,16 @@
+<?php
+?><div class="span2 main-menu-span">
+				<div class="well nav-collapse sidebar-nav">
+					<ul class="nav nav-tabs nav-stacked main-menu">
+						<li class="nav-header hidden-tablet">Main</li>
+
+						<li style="margin-left: -2px; "><a class="ajax-link" href="index.php"><i class="icon-home"></i><span class="hidden-tablet"> Dashboard</span></a></li>
+						<li style="margin-left: -2px; "><a class="ajax-link" href="index.php?title=employee"><i class="icon-eye-open"></i><span class="hidden-tablet">Employee</span></a></li>
+						<li style="margin-left: -2px; "><a class="ajax-link" href="index.php?title=student"><i class="icon-eye-open"></i><span class="hidden-tablet">Student</span></a></li>
+	                   	<li style="margin-left: -2px; "><a class="ajax-link" href="index.php?title=grade"><i class="icon-eye-open"></i><span class="hidden-tablet">Grade</span></a></li>
+	     				<li style="margin-left: -2px; "><a class="ajax-link" href="index.php?title=subject"><i class="icon-eye-open"></i><span class="hidden-tablet">subject</span></a></li>
+	                   	<li style="margin-left: -2px; "><a class="ajax-link" href="index.php?title=Mark information"><i class="icon-eye-open"></i><span class="hidden-tablet">Mark insertion</span></a></li>
+						<li style="margin-left: -2px; "><a class="ajax-link" href="index.php?title=pocket"><i class="icon-eye-open"></i><span class="hidden-tablet">Pocket</span></a></li>
+                    </ul>
+				</div><!--/.well-->
+			</div>
